7bb92851fdff6313e4ff40b35d89150ca83fe5c1
Codeayo <105753515+Codeayo@users.noreply.github.com>
2025-04-16 05:13:50 -0400

    Update db.js

--- JudgingApp/db.js
+++ JudgingApp/db.js
@@ -1,10 +1,16 @@
+// db.js
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host: process.env.DB_HOST || "mainline.proxy.rlwy.net",
+  user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT, // <- don't forget this
+  database: process.env.DB_NAME || "railway",
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 19692,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+module.exports = db;
